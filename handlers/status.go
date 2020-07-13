d1a9ab489e4e2da77646fe5cf13f14ada96985a4
John Shenk <john_shenk@hotmail.com>
2020-07-13 08:02:42 -0500

    debug api paths

--- handlers/status.go
+++ handlers/status.go
@@ -1,7 +1,11 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func Status(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("status called - path: ", r.URL.Path, " method: ", r.Method)
 	w.Write([]byte("OK"))
 }
